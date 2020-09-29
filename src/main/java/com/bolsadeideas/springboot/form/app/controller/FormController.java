527556c65589f011254b2292720d3c87cfe24b35
Christan Ariel Salazar Verduga <santisimoariel@gmail.com>
2020-09-29 23:31:27 +0200

    Metodo handler para recibir y procesar los datos enviados

--- src/main/java/com/bolsadeideas/springboot/form/app/controller/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/controller/FormController.java
@@ -4,21 +4,34 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Vamos a necesitar 2 metodos handler un GET para mostrar el formulario
  * <code>(@GetMapping)</code> y POST para procesar o recibir los datos del
  * formulario <code>(@PostMapping)</code>
+ * 
  */
 @Controller
 public class FormController {
 	@GetMapping("/form")
 	public String form(Model model) {
+		model.addAttribute("title","Form Usuario");
 		return "form";
 	}
 
+	/*
+	 * 1.- con @RequestParam T ImputName 2.- @RequestParam(name="ImputName") T
+	 * ImputName
+	 */
 	@PostMapping("/form")
-	public String procesar() {
-		return "resultado";
+	public String procesar(Model model, @RequestParam String username, @RequestParam String password,
+			@RequestParam String email) {
+		model.addAttribute("title","Resultado Form");
+		model.addAttribute("username",username);
+		model.addAttribute("password",password);
+		model.addAttribute("email",email);
+		
+		return "resultform";
 	}
 }
