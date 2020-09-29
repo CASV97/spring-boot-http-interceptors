7cbcd104e9c7f4e6abc0068162a546c0c5bfa8cc
Christan Ariel Salazar Verduga <santisimoariel@gmail.com>
2020-09-29 23:59:30 +0200

    Creando la clase modelo del formilario

--- src/main/java/com/bolsadeideas/springboot/form/app/controller/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/controller/FormController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.bolsadeideas.springboot.form.app.models.entity.User;
+
 /**
  * Vamos a necesitar 2 metodos handler un GET para mostrar el formulario
  * <code>(@GetMapping)</code> y POST para procesar o recibir los datos del
@@ -27,10 +29,11 @@ public class FormController {
 	@PostMapping("/form")
 	public String procesar(Model model, @RequestParam String username, @RequestParam String password,
 			@RequestParam String email) {
+		//clase pojo(una clase una entity que representa datos)
+		User user = new User(username,password,email);
 		model.addAttribute("title","Resultado Form");
+		model.addAttribute("user", user);
 		model.addAttribute("username",username);
-		model.addAttribute("password",password);
-		model.addAttribute("email",email);
 		
 		return "resultform";
 	}
